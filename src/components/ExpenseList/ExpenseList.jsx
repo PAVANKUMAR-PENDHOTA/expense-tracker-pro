ab5110d1daa5bbfaf88e015dbc036f25dc1d43b1
Pavankumar <pendhotapavankumar@gmail.com>
2026-07-15 23:51:33 +0530

    Modified ExpenseList to Display content

--- src/components/ExpenseList/ExpenseList.jsx
+++ src/components/ExpenseList/ExpenseList.jsx
@@ -3,12 +3,9 @@
 const ExpenseList = () => {
   return (
     <div>
-      <h1>Salary</h1>
-      <h3>$5000</h3>
-      <h3>Category: Income </h3>
-      <h3>Date: 2023-06-01 </h3>
-      <h3>Notes: Monthly salary</h3>
-
+      <h1>Expense List</h1>
+      <p>No expenses to display.</p>
+      
     </div>
   )
 }

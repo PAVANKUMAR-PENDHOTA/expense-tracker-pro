736af34cd912c0156686f2f0e1004bfe4d5f6f5e
Pavankumar <pendhotapavankumar@gmail.com>
2026-07-15 23:56:47 +0530

    Added Static data for display

--- src/components/ExpenseList/ExpenseList.jsx
+++ src/components/ExpenseList/ExpenseList.jsx
@@ -5,7 +5,14 @@ const ExpenseList = () => {
     <div>
       <h1>Expense List</h1>
       <p>No expenses to display.</p>
-      
+      <div>
+        <ul className="list-group">
+            <li className="list-group-item">Expense 1</li>
+            <li className="list-group-item">Expense 2</li>
+            <li className="list-group-item">Expense 3</li>
+        </ul>
+      </div>
+
     </div>
   )
 }
